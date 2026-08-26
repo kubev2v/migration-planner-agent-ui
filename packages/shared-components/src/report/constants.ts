c70b01f57440a2b7c46efab52e4771f97388029f
Guillaume Vincent <gvincent@redhat.com>
2026-08-26 15:50:52 +0200

    chore: Add missing keys used in ui-app
    Signed-off-by: Guillaume Vincent <gvincent@redhat.com>

--- packages/shared-components/src/report/constants.ts
+++ packages/shared-components/src/report/constants.ts
@@ -10,5 +10,17 @@ export const chartColorSuccess = chart_color_blue_300.value;
 export const chartColorFailure = chart_color_yellow_300.value;
 
 export const REPORT_CARD_EMPTY_STATE_TITLES = {
+  networks: "Network data not collected",
+  nicCount: "NIC count data not collected",
+  hosts: "Host data not collected",
+  cpuMemory: "CPU and memory data not collected",
+  cpu: "CPU data not collected",
+  memory: "Memory data not collected",
+  storage: "Storage data not collected",
+  diskTypes: "Disk type data not collected",
+  clusters: "Cluster data not collected",
+  cpuOvercommitment: "CPU overcommitment data not collected",
   operatingSystems: "Operating system data not collected",
+  migrationStatus: "Migration status data not collected",
+  issuesBreakdown: "Issues breakdown data not collected",
 } as const;
